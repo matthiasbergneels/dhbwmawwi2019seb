80f91d47195088400987016b347627ae8e47c389
Matthias Berg-Neels <matthias.uwe.berg-neels@sap.com>
2020-01-13 16:41:32 +0100

    polymorphie

--- src/chapter6/Farm.java
+++ src/chapter6/Farm.java
@@ -24,6 +24,44 @@ public class Farm {
         System.out.println(myDog.toString());
         System.out.println(myDog);
 
+        // narrowing Cast --> Sichtbarkeit auf das Objekt wird eingeschränkt
+        Animal myAnimal = myDog;
+
+        myAnimal.breath();
+
+        Object myObject = myDog;
+
+
+        Animal myOtherAnimal = new Bird(9.0f, 10.0f, "Tweety", true);
+
+        //myOtherAnomal.twee
+
+
+        //widening cast --> Erweitern der Sichtbarkeit auf ein Objekt
+        Dog nextDog = (Dog)myAnimal;
+
+        nextDog.bark();
+
+
+        Animal[] animalShelter = new Animal[5];
+
+        animalShelter[0] = myDog;
+        animalShelter[1] = myOtherAnimal;
+        animalShelter[2] = new Dog(25.5f, 30.0f, "Wuffi", "Irgendwas");
+        animalShelter[3] = new Bird(13, 10, "Piep", true);
+        animalShelter[4] = new Bird(13, 10, "PiepPiep", false);
+
+
+        for(Animal currentAnimal : animalShelter){
+            currentAnimal.breath();
+            currentAnimal.eat();
+            currentAnimal.move();
+
+            if(currentAnimal instanceof Dog) {
+                Dog currentDog = (Dog) currentAnimal;
+                currentDog.bark();
+            }
+        }
 
 
     }
