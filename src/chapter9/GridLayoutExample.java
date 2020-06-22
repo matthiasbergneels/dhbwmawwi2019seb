f400ae3f72e8015e102c2a125f780bbbd8882f1d
Matthias Berg-Neels <matthias.uwe.berg-neels@sap.com>
2020-06-22 18:13:25 +0200

    GridLayout Example

--- src/chapter9/GridLayoutExample.java
+++ src/chapter9/GridLayoutExample.java
@@ -1,28 +1,39 @@
 package chapter9;
 
+import org.junit.jupiter.engine.descriptor.JupiterTestDescriptor;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.concurrent.Flow;
 
 public class GridLayoutExample extends JFrame {
 
     private GridLayoutExample(){
         super("GridLayout example");
 
-        JPanel mainPanel = new JPanel(new GridLayout());
+        JPanel mainPanel = new JPanel(new GridLayout(0,2));
 
-        JButton firstButton = new JButton("First");
-        JButton secondButton = new JButton("Second");
-        JButton thirdButton = new JButton("Third");
-        JButton fourthButton = new JButton("fourth");
-        JButton fifthButton = new JButton("fifth");
+        JLabel salutationLabel = new JLabel("Anrede:");
+        JComboBox<String> salutationComboBox = new JComboBox<String>();
+        salutationComboBox.addItem("Herr");
+        salutationComboBox.addItem("Frau");
+        salutationComboBox.addItem("Divers");
 
-        mainPanel.add(firstButton);
-        mainPanel.add(secondButton);
-        mainPanel.add(thirdButton);
-        mainPanel.add(fourthButton);
-        mainPanel.add(fifthButton);
+        JLabel familyNameLabel = new JLabel("Familienname:");
+        JTextField familyNameTextField = new JTextField(15);
+        JLabel nameLabel = new JLabel("Name: ");
+        JTextField nameTextField = new JTextField(15);
+        JButton saveButton = new JButton("Sichern");
+        JButton cancelButton = new JButton("Abbrechen");
 
-
+        mainPanel.add(generateFlowLayoutPanelWithComponent(salutationLabel));
+        mainPanel.add(generateFlowLayoutPanelWithComponent(salutationComboBox));
+        mainPanel.add(generateFlowLayoutPanelWithComponent(familyNameLabel));
+        mainPanel.add(generateFlowLayoutPanelWithComponent(familyNameTextField));
+        mainPanel.add(generateFlowLayoutPanelWithComponent(nameLabel));
+        mainPanel.add(generateFlowLayoutPanelWithComponent(nameTextField));
+        mainPanel.add(generateFlowLayoutPanelWithComponent(saveButton));
+        mainPanel.add(generateFlowLayoutPanelWithComponent(cancelButton));
 
         this.add(mainPanel);
 
@@ -31,6 +42,11 @@ public class GridLayoutExample extends JFrame {
         this.setVisible(true);
     }
 
+    private JPanel generateFlowLayoutPanelWithComponent(JComponent component){
+        JPanel cellPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        cellPanel.add(component);
+        return cellPanel;
+    }
 
     public static void main(String[] args) {
         new GridLayoutExample();
