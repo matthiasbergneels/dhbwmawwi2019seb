723f9d5acf6f7934335e47ad48d54a78b0b143d0
Matthias Berg-Neels <matthias.uwe.berg-neels@sap.com>
2020-06-22 17:42:46 +0200

    BorderLayout Example

--- src/chapter9/BorderLayoutExample.java
+++ src/chapter9/BorderLayoutExample.java
@@ -1,14 +1,33 @@
 package chapter9;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class BorderLayoutExample extends JFrame {
 
     private BorderLayoutExample(){
         super("BorderLayout example");
 
-        
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        JPanel centerPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
+        JButton northButton = new JButton("North");
+        JButton southButton = new JButton("South");
+        JButton westButton = new JButton("West");
+        JButton eastButton = new JButton("East");
+        JButton centerButton = new JButton("Center");
+        JButton newButton = new JButton("New Button!");
+
+        centerPanel.add(centerButton);
+        centerPanel.add(newButton);
+
+        mainPanel.add(northButton, BorderLayout.NORTH);
+        mainPanel.add(southButton, BorderLayout.SOUTH);
+        mainPanel.add(westButton, BorderLayout.WEST);
+        mainPanel.add(eastButton, BorderLayout.EAST);
+        mainPanel.add(centerPanel, BorderLayout.CENTER);
+
+        this.add(mainPanel);
 
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.pack();
