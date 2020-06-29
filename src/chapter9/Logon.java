69f6eb17516249522a8b875ed90a26385c8bc040
Matthias Berg-Neels <matthias.uwe.berg-neels@sap.com>
2020-06-29 15:10:24 +0200

    Logon UI

---
+++ src/chapter9/Logon.java
@@ -0,0 +1,135 @@
+package lecture.chapter9;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.text.ParseException;
+
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+import javax.swing.text.MaskFormatter;
+
+public class Logon extends JFrame{
+
+    private static final String COMMAND_OK = "OK";
+    private static final String COMMAND_CANCEL = "CANCEL";
+
+    private JTextField host;
+
+    public Logon() throws ParseException{
+
+        this.setTitle("Logon");
+
+        String[] valueHelp = {"FTP", "Telnet", "SMTP", "HTTP"};
+        JComboBox<String> myComboBox = new JComboBox<String>(valueHelp);
+
+        myComboBox.addItemListener((ItemEvent e) -> {
+            if(e.getStateChange() == ItemEvent.SELECTED){
+                System.out.println("Selected item: " + e.getItem());
+            }
+        });
+
+        myComboBox.addActionListener((ActionEvent e) -> {
+            System.out.println(e.getActionCommand());
+        });
+
+
+
+        // initialize Panels
+        JPanel mainPanel = new JPanel(new BorderLayout());
+
+        JPanel southPanel = new JPanel(new FlowLayout());
+        JPanel centerPanel = new JPanel(new FlowLayout());
+
+        JPanel connectionPanel = new JPanel(new GridLayout(0, 2));
+        JPanel filePanel = new JPanel(new GridLayout(0, 2));
+
+        JFormattedTextField portField = new JFormattedTextField(new MaskFormatter("#####"));
+        portField.setColumns(3);
+
+        FlowLayout cellFlowLayout = new FlowLayout(FlowLayout.LEFT);
+
+        //create & assign elements for connection area
+        JPanel flowLayoutForCell = new JPanel(cellFlowLayout);
+        flowLayoutForCell.add(new JLabel("User:"));
+        connectionPanel.add(flowLayoutForCell);
+        flowLayoutForCell = new JPanel(cellFlowLayout);
+        flowLayoutForCell.add(new JTextField(3));
+        connectionPanel.add(flowLayoutForCell);
+        flowLayoutForCell = new JPanel(cellFlowLayout);
+        flowLayoutForCell.add(new JLabel("Passwort:"));
+        connectionPanel.add(flowLayoutForCell);
+        flowLayoutForCell = new JPanel(cellFlowLayout);
+        flowLayoutForCell.add(new JPasswordField(5));
+        connectionPanel.add(flowLayoutForCell);
+        flowLayoutForCell = new JPanel(cellFlowLayout);
+        flowLayoutForCell.add(new JLabel("Art:"));
+        connectionPanel.add(flowLayoutForCell);
+        flowLayoutForCell = new JPanel(cellFlowLayout);
+        flowLayoutForCell.add(myComboBox);
+        connectionPanel.add(flowLayoutForCell);
+        flowLayoutForCell = new JPanel(cellFlowLayout);
+        flowLayoutForCell.add(new JLabel("Host:"));
+        connectionPanel.add(flowLayoutForCell);
+        flowLayoutForCell = new JPanel(cellFlowLayout);
+        host = new JTextField(5);
+        flowLayoutForCell.add(host);
+        connectionPanel.add(flowLayoutForCell);
+        flowLayoutForCell = new JPanel(cellFlowLayout);
+        flowLayoutForCell.add(new JLabel("Port:"));
+        connectionPanel.add(flowLayoutForCell);
+        flowLayoutForCell = new JPanel(cellFlowLayout);
+        flowLayoutForCell.add(portField);
+        connectionPanel.add(flowLayoutForCell);
+
+        // create & add Fields for File Area
+        flowLayoutForCell = new JPanel(cellFlowLayout);
+        flowLayoutForCell.add(new JLabel("Quelle:"));
+        filePanel.add(flowLayoutForCell);
+        flowLayoutForCell = new JPanel(cellFlowLayout);
+        flowLayoutForCell.add(new JTextField(10));
+        filePanel.add(flowLayoutForCell);
+        flowLayoutForCell = new JPanel(cellFlowLayout);
+        flowLayoutForCell.add(new JLabel("Ziel:"));
+        filePanel.add(flowLayoutForCell);
+        flowLayoutForCell = new JPanel(cellFlowLayout);
+        flowLayoutForCell.add(new JTextField(10));
+        filePanel.add(flowLayoutForCell);
+
+        // create & assign Buttons
+        JButton okButton = new JButton("Okay");
+        JButton cancelButton = new JButton("Cancel");
+
+        southPanel.add(okButton);
+        southPanel.add(cancelButton);
+
+        // create & assign Borders
+        Border etchedBorder = BorderFactory.createEtchedBorder();
+        Border connectionBorder = BorderFactory.createTitledBorder(etchedBorder, "Verbindung");
+        Border fileBorder = BorderFactory.createTitledBorder(etchedBorder, "Datei");
+        Border centerBorder = BorderFactory.createBevelBorder(BevelBorder.LOWERED);
+
+        connectionPanel.setBorder(connectionBorder);
+        filePanel.setBorder(fileBorder);
+        centerPanel.setBorder(centerBorder);
+
+        // combine Panels
+        centerPanel.add(connectionPanel);
+        centerPanel.add(filePanel);
+
+        mainPanel.add(centerPanel, BorderLayout.CENTER);
+        mainPanel.add(southPanel, BorderLayout.SOUTH);
+
+        this.setContentPane(mainPanel);
+
+        // set JFrame behavior
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) throws ParseException {
+        new Logon();
+    }
+
+}
